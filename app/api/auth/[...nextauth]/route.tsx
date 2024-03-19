5614ee816e5257ffe183cd1585db6060eab64331
ElvisMuresan <elvis.e.muresan@gmail.com>
2024-03-19 12:10:54 +0200

    feat: Added the clientId and clientSecret credentials for google

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,19 +1,27 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
+console.log({
+  clientId: process.env.GOOGLE_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+});
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: "",
-      clientSecret: "",
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  //   async session({session}) {
+  // async session({session}) {
+
+  // }
+  // async signIn({profile}) {
+  //   try {
+
+  //   } catch (error) {
 
   //   }
-  //   async signIn({profile}) {
-
-  //   }
+  // }
 });
 
 export { handler as GET, handler as POST };
