ff362af539be643087b34011cdd9eee9888b79ff
ElvisMuresan <elvis.e.muresan@gmail.com>
2024-03-19 15:18:17 +0200

    fead: Done implementing the route tha's going to handle the entire authentification process

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
+import User from "@app/models/user";
 import { connectTODB } from "@utils/database";
 
 console.log({
@@ -14,10 +15,29 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }: any) {},
+  async session({ session }: any) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+
+    session.user.id = sessionUser._id.toString();
+    return session;
+  },
   async signIn({ profile }: any) {
     try {
       await connectTODB();
+
+      //check if a user already exists
+      const userExists = await User.findOne({ email: profile.email });
+
+      //if not, create a new user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
     } catch (error) {}
   },
 });
